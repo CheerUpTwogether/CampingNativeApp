89ac51f8c7a29456bc5f836c3acba118f5297462
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-08-03 21:23:19 +0900

    [feat]#30 Article 페이지 탑바, 소개 이미지 추가

--- src/pages/articles/Articles.tsx
+++ src/pages/articles/Articles.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import TopBar from "@/components/common/TopBar";
+
+const menu = require("@/assets/icons/menu.png");
+const profile = { uri: "https://picsum.photos/200/300" };
+const ArticleInfoImg = require("@/assets/images/ArticleInfo.png");
 
 const Articles = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
-      <View>
+      <TopBar
+        title="아티클"
+        leftIcon={menu}
+        rightIsProfile={true}
+        rightIcon={profile}
+      />
+      <View style={styles.container}>
+        <Image source={ArticleInfoImg} style={styles.ArticleInfoImg} />
         <Text>Articles</Text>
       </View>
     </SafeAreaView>
@@ -15,6 +27,14 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: "#FFF3E9",
+  },
+  container: {
+    padding: 12,
+  },
+  ArticleInfoImg: {
+    height: 150,
+    width: "100%",
+    resizeMode: "contain",
   },
 });
 
