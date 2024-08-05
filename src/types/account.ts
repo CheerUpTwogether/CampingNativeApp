3bb8c0d17ccd074bc1f40437131c7917175008ba
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-08-05 14:51:46 +0900

    [refactor]#54 Signup 타입 에러 수정 및 아이콘 변경

---
+++ src/types/account.ts
@@ -0,0 +1,13 @@
+// 사용자 인터페이스 정의
+interface User {
+  email: string;
+  password: string;
+}
+
+// 사용자 인터페이스 정의
+interface UserDetail {
+  email: string;
+  password: string;
+  nickname: string;
+  phoneNumber: string;
+}
